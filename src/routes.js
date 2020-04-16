18da9bc3b49dd2034b33ff6542f10917d4fbf49a
Fernando Jorge <fernando.junior@ccc.ufcg.edu.br>
2020-04-16 16:40:34 -0300

    end-1

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { createStackNavigator } from 'react-navigation'
 
 import Maim from './pages/main'
+import Product from './pages/product'
 
 export default createStackNavigator({
-    Maim
-},{
+    Maim,
+    Product
+}, {
     navigationOptions: {
         headerStyle: {
             backgroundColor: '#DA552F',
